af6288a43a04656bb45dd185d6862dc180961384
shivprasad murgude <murgude.shivpa@gmail.com>
2022-09-07 16:24:15 +0530

    issue related to select un select of assembly check box for loading and clearing data in booth is solved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ElectionService } from './election.service';
 import { Election } from './model/election';
 import { Responce } from './model/responce';
@@ -12,29 +12,30 @@ import { Responce } from './model/responce';
 export class AppComponent implements OnInit {
   title = 'task - 2';
 
+  electionFrom!: FormGroup;
+
   // Election
-  electionList!: any[];
+  electionList: any[] = [];
 
   electionSelected!: number;
 
   // Constituency
-  constituencyList!: any[];
+  constituencyList: any[] = [];
 
   constituencySelected!: number;
 
   // Assembly
-  assemblyList!: any[];
+  assemblyList: any[] = [];
+  ;
 
   assemblySelectedArray: any[] = [];
-
-  currentAssemblyLength: number = 0;
 
   // Booth
   boothList: any[] = [];
 
   boothSelected!: any[];
 
-  form!: FormGroup;
+
 
   electionData: any = [{}];
 
@@ -44,8 +45,13 @@ export class AppComponent implements OnInit {
 
 
   constructor(private electionService: ElectionService, private formBuilder: FormBuilder) {
-    this.form = this.formBuilder.group({
-      assemblyList: []
+    this.electionFrom = this.formBuilder.group({
+      electionId: ['0', Validators.required],
+      constituencyId: ['0', Validators.required],
+
+      assemblyList: this.formBuilder.array([]),
+      boothList: this.formBuilder.array([])
+
     })
   }
 
@@ -89,47 +95,49 @@ export class AppComponent implements OnInit {
     })
   }
 
-  // use constituency dropdown to get constituencyId
+  // called when assembly checkbox checked or unchecked
+  // it loads data in booth depending upon is any checkbox checked or not checked
   loadBooth() {
     if (this.assemblySelectedArray.length !== 0) {
-      if (this.assemblySelectedArray.length !== this.currentAssemblyLength) {
-
-        this.currentAssemblyLength = this.assemblySelectedArray.length;
-
-        this.assemblySelectedArray.forEach(assembly => {
-
-          // console.log(assembly)
-          // console.log(assembly.Id)
-          this.boothList.length = 0;
-
-          this.electionService.getBooth(assembly.Id).subscribe(response => {
-            for (let prop in response) {
-              if (response[prop] !== 0) {
-                this.boothList.push(...response[prop]);
-                console.log(`Booth length `, this.boothList.length);
-              }
+      this.assemblySelectedArray.forEach(assembly => {
+        this.electionService.getBooth(assembly.Id).subscribe(response => {
+          for (let prop in response) {
+            if (response[prop] !== 0) {
+              this.boothList.push(...response[prop]);
             }
-          })
+          }
         })
-      }
+      })
+
     }
-
   }
-
 
   assemblySubmit() { }
   onConstituencySelect() {
-
   }
-
 
   // use to gather data and push it data source
   displayData() {
     console.log('disply  clicked');
   }
 
-  getChecked(data: any) {
-    this.assemblySelectedArray.push(data);
+  // also reset boothList array to load new responce data
+  // on cheeckbox check or uncheck event
+  // if isChecked
+  // it push the value inside assembly array
+  // then it calles loadBooth() which makes http request based on id
+  // if isUnchecked
+  // get the index of assembly array
+  // take that element out
+  // run loadBooth()
+  getChecked(data: any, id: string, isChecked: boolean) {
+    if (isChecked) {
+      this.assemblySelectedArray.push({ ...data, id });
+    } else {
+      let index = this.assemblySelectedArray.findIndex(obj => { return obj.id === id })
+      this.assemblySelectedArray.splice(index, 1);
+    }
+    this.boothList.length = 0;
     this.loadBooth()
   }
 
